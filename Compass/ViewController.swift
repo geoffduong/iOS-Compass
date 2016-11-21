591c26f708862d9317133e4f1bdaaa2d4cc7c8ad
mrricearoni <duongg4848@gmail.com>
2016-11-21 14:37:55 -0500

    Image rotation working with button Just testing

--- Compass/ViewController.swift
+++ Compass/ViewController.swift
@@ -8,13 +8,20 @@
 
 import UIKit
 import AVFoundation
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let speechSynthesizer = AVSpeechSynthesizer()
+    var locationManager:CLLocationManager!
+    var degrees:CGFloat = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager  = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.startUpdatingHeading()
         
         
         
@@ -29,17 +36,40 @@ class ViewController: UIViewController {
     }
     
     @IBOutlet weak var compassImage: UIImageView!
+    @IBOutlet weak var headingLabel: UILabel!
+    @IBOutlet weak var rotateBtn: UIButton!
+    
+    //TEST: rotate image
+    @IBAction func rotateImage(_ sender: UIButton) {
+        degrees += CGFloat(M_PI)/2.0
+        UIView.animate(withDuration: 2.0, animations: {
+            self.compassImage.transform = CGAffineTransform(rotationAngle: self.degrees)
+        })
+    }
     
     
+    
+    
+    
+    
+    
+    //Speak if immage is tapped
     func imageTapped(img: AnyObject)
     {
-        speak("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW")
+        speak("WWWWW")
     }
     
+    //Text to speech
     func speak(_ sender: String) {
         let speechUtterance = AVSpeechUtterance(string: sender)
-        
         speechSynthesizer.speak(speechUtterance)
     }
+    
+    //Gets heading of device using location
+    private func locationManager(manager: CLLocationManager!, didUpdateHeading heading: CLHeading!) {
+        // This will print out the direction the device is heading
+        headingLabel.text = heading.magneticHeading.description
+    }
 }
+
 
